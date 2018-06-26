53c586cf804eca8eeddeaf42375246fdbd8cc5b6
Guilherme Conti <guibasconti@gmail.com>
2018-06-25 22:21:35 -0300

    Change response

--- server/core/router.js
+++ server/core/router.js
@@ -27,7 +27,7 @@ const HelloWorldIntentHandler = {
           && handlerInput.requestEnvelope.request.intent.name === 'GetNewFactIntent';
   },
   handle(handlerInput) {
-      const speechText = 'Why don\'t you google yourself? XABLAU';
+      const speechText = 'Hey dude it just works, congratulations! XABLAU';
 
       return handlerInput.responseBuilder
           .speak(speechText)
