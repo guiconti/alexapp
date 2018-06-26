4a6373d6a2e4643f82357b7e4ec937613bb032ef
Guilherme Conti <guibasconti@gmail.com>
2018-06-25 21:27:09 -0300

    Test response

--- server/core/router.js
+++ server/core/router.js
@@ -11,7 +11,44 @@ router.get('/', (req, res) => {
 
 router.post('*', (req, res) => {
   console.log('Hi');
-  res.status(200).json({ msg: 'hi '});
+  res.status(200).json(
+    {
+      "version": "string",
+      "sessionAttributes": {
+        "key": "value"
+      },
+      "response": {
+        "outputSpeech": {
+          "type": "PlainText",
+          "text": "Plain text string to speak",
+          "ssml": "<speak>SSML text string to speak</speak>"
+        },
+        "card": {
+          "type": "Standard",
+          "title": "Title of the card",
+          "content": "Content of a simple card",
+          "text": "Text content for a standard card",
+          "image": {
+            "smallImageUrl": "https://url-to-small-card-image...",
+            "largeImageUrl": "https://url-to-large-card-image..."
+          }
+        },
+        "reprompt": {
+          "outputSpeech": {
+            "type": "PlainText",
+            "text": "Plain text string to speak",
+            "ssml": "<speak>SSML text string to speak</speak>"
+          }
+        },
+        "directives": [
+          {
+            "type": "InterfaceName.Directive"
+          }
+        ],
+        "shouldEndSession": true
+      }
+    }
+  );
 })
 
 module.exports = router;
