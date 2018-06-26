d1817e9bf427b32f1ddf09bdf48cbe111ac00da7
Guilherme Conti <guibasconti@gmail.com>
2018-06-25 21:42:37 -0300

    Add alexa SDK

--- server/core/router.js
+++ server/core/router.js
@@ -4,6 +4,94 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 router.use(bodyParser.json());
 
+const Alexa = require('alexa-sdk');
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'LaunchRequest';
+  },
+  handle(handlerInput) {
+      const speechText = 'Welcome to the Alexa Skills Kit, you can say hello!';
+
+      return handlerInput.responseBuilder
+          .speak(speechText)
+          .reprompt(speechText)
+          .withSimpleCard('Hello World', speechText)
+          .getResponse();
+  }
+};
+
+const HelloWorldIntentHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+          && handlerInput.requestEnvelope.request.intent.name === 'HelloWorldIntent';
+  },
+  handle(handlerInput) {
+      const speechText = 'Hello World!';
+
+      return handlerInput.responseBuilder
+          .speak(speechText)
+          .withSimpleCard('Hello World', speechText)
+          .getResponse();
+  }
+};
+
+const HelpIntentHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+          && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+      const speechText = 'You can say hello to me!';
+
+      return handlerInput.responseBuilder
+          .speak(speechText)
+          .reprompt(speechText)
+          .withSimpleCard('Hello World', speechText)
+          .getResponse();
+  }
+};
+
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+          && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+              || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+      const speechText = 'Goodbye!';
+
+      return handlerInput.responseBuilder
+          .speak(speechText)
+          .withSimpleCard('Hello World', speechText)
+          .getResponse();
+  }
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+      return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+      //any cleanup logic goes here
+      return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
 //  Placeholder API
 router.get('/', (req, res) => {
   res.status(200).json({ msg: 'Hi!' });
@@ -11,44 +99,38 @@ router.get('/', (req, res) => {
 
 router.post('*', (req, res) => {
   console.log('Hi');
-  res.status(200).json(
-    {
-      "version": "string",
-      "sessionAttributes": {
-        "key": "value"
-      },
-      "response": {
-        "outputSpeech": {
-          "type": "PlainText",
-          "text": "Plain text string to speak",
-          "ssml": "<speak>SSML text string to speak</speak>"
-        },
-        "card": {
-          "type": "Standard",
-          "title": "Title of the card",
-          "content": "Content of a simple card",
-          "text": "Text content for a standard card",
-          "image": {
-            "smallImageUrl": "https://url-to-small-card-image...",
-            "largeImageUrl": "https://url-to-large-card-image..."
-          }
-        },
-        "reprompt": {
-          "outputSpeech": {
-            "type": "PlainText",
-            "text": "Plain text string to speak",
-            "ssml": "<speak>SSML text string to speak</speak>"
-          }
-        },
-        "directives": [
-          {
-            "type": "InterfaceName.Directive"
-          }
-        ],
-        "shouldEndSession": true
-      }
+  let context = {
+    succeed: function (result) {
+      console.log(result);
+      res.json(result);
+    },
+    fail:function (error) {
+      console.log(error);
     }
-  );
+  };
+  // let alexa = Alexa.handler(req.body, context);
+  // alexa.registerHandlers(handlers);
+  // alexa.execute();
+
+  let skill = Alexa.SkillBuilders.custom()
+  .addRequestHandlers(
+    LaunchRequestHandler,
+    HelloWorldIntentHandler,
+    HelpIntentHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+  )
+  .addErrorHandlers(ErrorHandler)
+  .create();
+
+  skill.invoke(req.body, context)
+      .then(function(responseBody) {
+        res.json(responseBody);
+      })
+      .catch(function(error) {
+        console.log(error);
+        res.status(500).send('Error during the request');
+      });
 })
 
 module.exports = router;
